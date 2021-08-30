4a615901dc9dd4d7de25297b2da756139c299d29
Vinicio Shurman <48026024+vinicioshurman@users.noreply.github.com>
2021-08-30 17:27:56 -0300

    Update exercicio4.php

--- PW 2/atividade01-09-2021/exercicio4.php
+++ PW 2/atividade01-09-2021/exercicio4.php
@@ -2,6 +2,6 @@
     //Usando for, faça um script para contar de 10 até 1 e imprima o resultado na tela na cor verde.
 
     for($num = 10; $num >= 1; $num--){
-        echo $num, '<br>';
+        echo '<p style="color: green; font-size: 3rem;"><strong>', $num, '<strong><p>';
     }
 ?>
