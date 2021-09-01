1364ca4f8dce41d80cb376532ab8bf967f2b9f22
Vinicio Shurman <48026024+vinicioshurman@users.noreply.github.com>
2021-08-31 23:10:38 -0300

    Atividade finalizada

--- Programação de Aplicativos Mobile 1/atividade02-09-2021/exercicio1e2.dart
+++ Programação de Aplicativos Mobile 1/atividade02-09-2021/exercicio1e2.dart
@@ -90,12 +90,64 @@ class Produto{
   } 
   
   //Metodos
-  bool comprar(Produto produto){
+  bool comprar(int quantidade){
     bool retorno = false;
-    if(produto.getQuantidadeEstoque => 1){
-      this._quantidadeEstoque;
+    if(quantidade > 0){
+      this._quantidadeEstoque += quantidade;
+      retorno = true;
     }
+    return retorno; 
   }
+  bool vender(int quantidade){
+    bool retorno = false;
+    if(quantidade > 0 && quantidade <= this._quantidadeEstoque){
+      this._quantidadeEstoque -= quantidade;
+      retorno = true;
+    }
+    return retorno;
+  }
+  String getValorEstoque(){
+    String valorEstoque;
+    valorEstoque =  (this._precoVenda * this._quantidadeEstoque).toString();
+    return valorEstoque;
+  }
+  String toString(){
+    return "Nome: $_nome Código de barra: $_codigoBarra Quantidade em estoque: $_quantidadeEstoque Preço de venda: $_precoVenda";
+  }
+  bool equals(Produto produto){
+    bool retorno = false;
+    if(this._codigoBarra == produto.getCodigoBarra() && this._precoVenda == produto.getPrecoVenda() && this._quantidadeEstoque == produto.getQuantidadeEstoque()){
+      retorno = true;
+    }
+    return retorno;
+  }
+}
+
+void main(){
+  //Gatos
+  Gato tom = new Gato('Tom', 'Persa', 'Cinza', 8);
+  Gato simba = new Gato('Simba', 'Siamês', 'Preto', 7);
+  Gato fred = new Gato('Fred', 'Ragdoll', 'Caramelo', 18);
+  Gato mingau = new Gato('Mingau', 'Sphynx', 'Cinza', 6);
+  Gato theo = new Gato('Theo', 'Persa', 'Caramelo', 3);
+  Gato gato = new Gato('Gato', 'Ragdoll', 'Laranja', 1);
+  
+  print(tom.miar());
+  print(simba.ronronar());
+  print(fred.dormir());
+  print(mingau.comer());
+  print(theo.fazerXixi());
+  print(gato.fazerCoco());
+  
+  //Produtos
+  Produto arroz = new Produto('Arroz Bentinho', '40821498', 30, 100);
+  Produto feijao = new Produto('Feijão Bentinho', '41235637', 15, 80);
+  
+  print(arroz.comprar(6));
+  print(arroz.vender(3));
+  print(arroz.getValorEstoque());
+  print(arroz.toString());
+  print(arroz.equals(feijao));
 }
   
  
